f0f38efd4c9605abb02002a742d75f5a59b132c0
Fivemindz <JD>
2020-11-08 16:13:21 -0600

    updates

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,22 @@
 function handleSubmit(event) {
     event.preventDefault()
 
+    const url = "http://api.openweathermap.org/data/2.5/weather?zip="
+    const key = "&appid=64716188118a12cc44b50c1524afbe76"
+
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(url + formText + key)
     .then(res => {
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        console.log(data)
+        document.getElementById('results').innerHTML = data.main['temp']
     })
 }
 
